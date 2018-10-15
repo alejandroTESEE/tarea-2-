05091ff0803b0892ff0816562c4954bf11f60032
alejandro <43358869+alejandroTESEE@users.noreply.github.com>
2018-10-14 19:08:51 -0500

    Add files via upload

---
+++ tarea 2...cpp
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+
+int main (void)
+
+{
+    int a;
+    int n;
+    a=10,20,30,40,50,n;
+    printf("introduce el valor de a : ");
+    scanf("%d",&a);
+    if(a<70)
+    printf("continua con tus estudios debes esforzarte mas");
+    else
+    printf("felicidades has aprobado");
+    
+    getch ();
+    return 0;
+}
+
+    
